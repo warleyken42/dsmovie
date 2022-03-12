48a1fb846741b795147019f561b75fba95ab470f
Warley Kennedy <warley-ft@hotmail.com>
2022-03-11 23:24:58 -0300

    Save score

--- backend/src/main/java/com/devsuperior/dsmovie/repositories/MovieRepository.java
+++ backend/src/main/java/com/devsuperior/dsmovie/repositories/MovieRepository.java
@@ -5,5 +5,4 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import com.devsuperior.dsmovie.entities.Movie;
 
 public interface MovieRepository extends JpaRepository<Movie, Long>{
-	
 }
